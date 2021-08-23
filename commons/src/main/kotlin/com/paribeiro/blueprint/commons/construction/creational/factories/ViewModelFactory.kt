3b5ac148a6fc762916e79543756bf47eec30b6c2
Paulo Ribeiro <paulo.ribeiro10@gmail.com>
2021-08-23 16:36:42 +0100

    [Support] Dependency updates.

--- commons/src/main/kotlin/com/paribeiro/blueprint/commons/construction/creational/factories/ViewModelFactory.kt
+++ commons/src/main/kotlin/com/paribeiro/blueprint/commons/construction/creational/factories/ViewModelFactory.kt
@@ -8,7 +8,7 @@ import javax.inject.Provider
 class ViewModelFactory @Inject constructor(private val creators: Map<Class <out ViewModel>,
         @JvmSuppressWildcards Provider<ViewModel>>) : ViewModelProvider.Factory {
 
-    override fun <T : ViewModel?> create(modelClass: Class<T>): T {
+    override fun <T : ViewModel> create(modelClass: Class<T>): T {
         val creator = creators[modelClass] ?: creators.asIterable().firstOrNull {
             modelClass.isAssignableFrom(it.key)
         }?.value ?: throw IllegalArgumentException()
